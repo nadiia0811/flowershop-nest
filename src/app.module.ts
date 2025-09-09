3c84913e48523d22fc472569738cfbf467f76580
Nadiia Krzesinska <nb1985@ukr.net>
2025-09-09 06:44:52 +0000

    chore: initial project setup

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { FlowersModule } from './flowers/flowers.module';
+
+@Module({
+  imports: [FlowersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
