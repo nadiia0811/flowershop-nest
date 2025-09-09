292c4f89077f2dbca69bfc3e39ab9897db338806
Nadiia Krzesinska <nb1985@ukr.net>
2025-09-09 08:13:46 +0000

    feat(pipe): add custom ParseIntPipe and apply in flowers.controller

--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { FlowersService } from './flowers.service';
+import { ParseIntPipe } from 'src/conception/pipe';
 
 @Controller('flowers')
 export class FlowersController {
   constructor(private readonly flowersService: FlowersService) {}
+
+  @Get()
+  findAll(@Query('pageNumber', ParseIntPipe) pageNumber: number) { 
+    console.log(pageNumber)
+    return this.flowersService.findAll();
+  }
 }
